0624c76b463ac564b1c8055c21cd61db67834c53
tony <wd97411002@gmail.com>
2021-01-03 18:27:08 +0800

    Merge branch 'master' of https://github.com/horacio195511/Virtual_room_backend
    # Conflicts:
    #	room/views.py

--- room/views.py
+++ room/views.py
@@ -841,13 +841,16 @@ def meeting_update_view(request):
 @csrf_exempt
 def meeting_delete_view(request):
     meeting = Meeting.objects.get(pk=request.POST['id'])
+    topic = request.POST["topic"]
+    start = request.POST["start"]
+    room = request.POST["room"]
     attendee = meeting.attendee
     attendeelist = attendee.split(str=",")
     for i in attendeelist:
         user = User.objects.get(lastname=i)
         email = EmailMessage(
-            'Test',  # 電子郵件標題
-            '123',  # 電子郵件內容
+            '您已被取消邀請'+topic,  # 電子郵件標題
+            '會議時間為：' + start + '\n會議地點為：NTUST TR ' + room,  # 電子郵件內容
             settings.EMAIL_HOST_USER,  # 寄件者
             [user.mail]  # 收件者
         )
